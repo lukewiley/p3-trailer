700cda886aeb73281b6374b8b2f1f61f74307767
Wiley <lukew@it-rpub-mac13.eastern.ewu.edu>
2015-05-22 13:50:42 -0700

    Version 5

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,13 @@
 // Media Element Player
 
 $('audio,video').mediaelementplayer({
+  
 	success: function(player, node) {
 	  $('#' + node.id + '-mode').html('mode: ' + player.pluginType);
 	},
 	startLanguage: 'en',
 	translationSelector: true
+
 });
 
 
